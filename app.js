75145546ea5fd1eb474aab67bc4a8abd55616374
DimaKashyrin <kashyrindima@gmail.com>
2021-10-01 14:22:44 +0300

    fixed bug Ver.1.0.1

--- app.js
+++ app.js
@@ -1,25 +1,34 @@
-const path =require('path');
+const path = require('path');
 const fs = require('fs');
-const boysPath = path.join(__dirname,'boys');
-const girlsPath = path.join(__dirname,'girls');
 
-const getSort = (usersPathFirst,gender,usersPathSecond) => {
-  fs.readdir(usersPathFirst,((err,data) => {
-    if(err){
+const boysPath = path.join(__dirname, 'boys');
+const girlsPath = path.join(__dirname, 'girls');
+
+const getSort = (usersPathFirst, gender, usersPathSecond) => {
+  
+  fs.readdir(usersPathFirst, ((err, data) => {
+    if (err) {
       console.log(err);
       return;
     }
+    
     data.forEach(fileName => {
-      fs.readFile(path.join(usersPathFirst,fileName),((err,data) => {
-        if(JSON.parse(data).gender !== gender){
+      
+      fs.readFile(path.join(usersPathFirst, fileName), ((err, data) => {
+        let currentGender = JSON.parse(data).gender;
+        
+        if (currentGender !== gender) {
           fs.rename(
-            path.join(usersPathFirst,fileName),
-            path.join(usersPathSecond,fileName),
+            path.join(usersPathFirst, fileName),
+            path.join(usersPathSecond, fileName),
             (err) => console.log(err)
           )
         }
+        
       }))
+      
     })
+    
   }))
 }
 
